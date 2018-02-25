8b30f9b804c25ca2cf68463bec12d71ca61326cd
heinerPerezL <35514268+heinerPerezL@users.noreply.github.com>
2018-02-25 15:38:32 -0600

    elemina muertos

--- public/components/services/usuarios.service.js
+++ public/components/services/usuarios.service.js
@@ -57,46 +57,36 @@
 
       return listaUsuarios;
     }
-  }
-<<<<<<< HEAD
-})();
-=======
+    function _agregarMuerto (pnuevoMuerto) {
+  let listaMuertos = _obtenerMuerto ();
+  //let respuesta = true;
+  listaMuertos.push (pnuevoMuerto);
 
-  
-    
+  localStorage.setItem ('muertosLS', JSON.stringify (listaMuertos));
 
-    function _agregarMuerto(pnuevoMuerto){
-      let listaMuertos = _obtenerMuerto();
-      //let respuesta = true; 
-      listaMuertos.push(pnuevoMuerto);
-      
-      localStorage.setItem('muertosLS', JSON.stringify(listaMuertos));
-
-/*
+  /*
       asyncLocalStorage.setItem('muertosLS', listaMuertos).then((response) =>{
         respuesta = response;
       });
       return respuesta;*/
-    }
+}
 
-    
-    function _obtenerMuerto(){
-      let listaMuertos = [];
-      let listaMuertosLocal = JSON.parse(localStorage.getItem("muertosLS"));
+function _obtenerMuerto () {
+  let listaMuertos = [];
+  let listaMuertosLocal = JSON.parse (localStorage.getItem ('muertosLS'));
 
-      if(listaMuertosLocal == null){
-        listaMuertos = [];
-      }else{
-        listaMuertosLocal.forEach(obj => {
-          
-          let objMuerto = new Muerto (obj.apodo, obj.edad, obj.genero, obj.tamanno);
+  if (listaMuertosLocal == null) {
+    listaMuertos = [];
+  } else {
+    listaMuertosLocal.forEach (obj => {
+      let objMuerto = new Muerto (obj.apodo, obj.edad, obj.genero, obj.tamanno);
 
-          listaMuertos.push(objMuerto);
-        })
-      }
+      listaMuertos.push (objMuerto);
+    });
+  }
 
-      return listaMuertos;
-    }
-  
+  return listaMuertos;
+}
+
+  }
 })();
->>>>>>> Heiner
