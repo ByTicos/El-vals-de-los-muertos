b46c6984e33f9186a10008cb079524b4d5495b94
heinerPerezL <35514268+heinerPerezL@users.noreply.github.com>
2018-02-24 18:05:52 -0600

    clases y servicios
    se crean clases y los servicios

--- public/clases.js
+++ public/clases.js
@@ -1,22 +1,85 @@
 // TODO Dentro de este archivo se crean los objetos con sus respectivos métodos
-class Cliente{
-  constructor(pCedula, pNombre1, pApellido1, pEdad){
-    this.cedula = pCedula;
-    this.nombre1 = pNombre1;
-    this.apellido1 = pApellido1;
-    this.edad = pEdad;
-    this.listaVehiculos = [];
+
+
+class Muerto{
+  constructor(pEdad, pGenero, pApodo, pTamanno){
+    this.edad = pEdad ;
+    this.genero = pGenero;
+    this.apodo = pApodo;
+    this.tamanno = pTamanno;
+    this.entierro= '';
+    this.listaRetoques=[];
+    this.fiesta = [];
   }
-  registrarVehiculo(pVehiculo){
-    this.listaVehiculos.push(pVehiculo);
+  registrarEntierros(pEntierro){
+    this.entierro=pEntierro;
+  }
+
+   registrarRetoques(pRetoques){
+    this.listaRetoques.push(pRetoques);
+  }
+
+  registrarFiesta(pFiesta){
+    this.fiesta=pFiesta;
   }
 }
 
-class Vehiculo{
-  constructor(pPlaca, pMarca, pCapacidad, pAnno){
-    this.placa = pPlaca ;
-    this.marca = pMarca;
-    this.capacidad = pCapacidad;
-    this.anno = pAnno;
+class Entierro {
+  constructor (pFecha, pHoraInicio, pHoraFin, pPrioridad) {
+    this.fecha = pFecha;
+    this.horaInicio = pHoraInicio;
+    this.horaFin = pHoraFin;
+    this.prioridad = pPrioridad;
+    this.lugar = '';
+  }
+
+  registrarLugar(pLugar){
+    this.lugar=pLugar;
+  }
+
+}
+
+class Retoques {
+  constructor (pTipo,pNombre,pCosto) {
+    this.tipo = pTipo;
+    this.nombre=pNombre;
+    this.costo=pCosto;
   }
 }
+
+class Fiestas {
+  constructor (pFecha, pDuracion, pAnimador, pCosto) {
+    this.fecha = pFecha;
+    this.duracion = pDuracion;
+    this.animador = pAnimador;
+    this.costo = pCosto;
+    this.animador = '';
+  }
+  registrarAnimador(pAnimador){
+    this.animador=pAnimador;
+  }
+}
+
+class Lugares {
+  constructor (pId,pLugar) {
+    this.id=pId;
+    this.lugar = pLugar;
+  }
+}
+
+class Animadores {
+  constructor (pId, pNombre,pApellido, pCosto) {
+    this.id = pId;
+    this.nombre = pNombre;
+    this.apellido = pApellido;
+    this.costo = pCosto;
+  }
+}
+
+
+
+
+
+
+
+
