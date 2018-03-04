620aaa82c30c4aec0b5d9d40a39409073d6bb20d
heinerPerezL <35514268+heinerPerezL@users.noreply.github.com>
2018-03-04 11:23:37 -0600

    actualizacion de muertos
    fallida

--- public/components/muertos/registroMuerto.controller.js
+++ public/components/muertos/registroMuerto.controller.js
@@ -30,6 +30,36 @@
      vm.nuevoMuerto = null;
     
     }
+    
+     
+    
+
+    vm.editarMuertos=(pApodoMuerto,pEdadMuerto,pGeneroMuerto,pTamannoMuerto)=>{
+      document.querySelector('#apodo').value=pApodoMuerto;
+      document.querySelector ('#edad').value = pEdadMuerto;
+      document.querySelector('#genero').value=pGeneroMuerto;
+      document.querySelector('#tamanno').value=pTamannoMuerto;
+      document.querySelector('#btnRegistrar').classList.add('ocultar');
+    }
+
+    function actualizar(){
+      document.querySelector ('#btnRegistrar').classList.remove ('ocultar');
+      document.querySelector ('#btnActualizar').classList.add ('ocultar');
+      
+      let sApodo = document.querySelector('#apodo').value;
+      let sEdad = document.querySelector ('#edad').value; 
+      let sGenero = document.querySelector('#genero').value;
+      let sTamanno = document.querySelector('#tamanno').value;
+      let listaMuertosModificada=[];
+      
+
+      listaMuertosModificada.push(sApodo,sEdad,sGenero,sTamanno);
+      actualizarMuerto(listaMuertosModificada);
+
+      listarMuertos();
+    }
+  
+
      function listarMuertos() {
       vm.listaMuertos = servicioUsuarios.getMuerto();
     }
